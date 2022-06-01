2e235d6207e93813c61792c1ccb1c8edf3454ce4
Tanvir Bashar <tbashar09@gmail.com>
2022-06-01 12:44:08 -0400

    update with admin.js

--- public/index.js
+++ public/index.js
@@ -1,10 +1,28 @@
 async function main() {
-
     let response = await fetch('http://localhost:3001/listBooks')
     let books = await response.json()
 
     books.forEach(renderBook)
 }
+console.log(main);
+// async function updateBook() {
+//     let response = await fetch('http://localhost:9001/updateBook', {
+//         method: "PATCH",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             "id": 3,
+//             "title": "Legends of Arathrae",
+//         }),
+//     });
+
+//     let updatedBook = await response.json();
+//     console.log(updatedBook);
+// }
+
+
+
 
 function renderBook(book) {
     let bookContainer = document.querySelector('.book-container')
@@ -26,3 +44,4 @@ function renderBook(book) {
 }
 
 main()
+// updateBook()
